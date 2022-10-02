0812ba35224266a265c5a2ea5a2150fab85a2bb1
17876388 <alchin.y.y@sberbank.ru>
2022-10-02 21:03:01 +0300

    hw01-gradle

---
+++ README.md
@@ -0,0 +1,5 @@
+# Курса OTUS ["Разработчик Java"](https://otus.ru/lessons/java-professional/?utm_source=github&utm_medium=free&utm_campaign=otus)
+Группа 2022-09
+
+### Студент
+Юрий Альчин
---
+++ hw01-gradle/src/main/java/ru/otus/professional/Application.java
@@ -0,0 +1,31 @@
+package ru.otus.professional;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Application {
+
+    public static final String MAP_KEY1 = "key1";
+    private static final List<String> LIST_KEY1 = Arrays.asList("valKey3", "valKey2", "valKey1");
+    private static final String KEY1_ELEM4 = "valKey4";
+
+    public static void main(String... args) {
+        ArrayListMultimap<String, String> mapOfLists = ArrayListMultimap.create();
+        mapOfLists.putAll(MAP_KEY1, LIST_KEY1);
+        mapOfLists.put(MAP_KEY1, KEY1_ELEM4);
+
+        log("before sort::%s%n", mapOfLists);
+        Collections.sort(mapOfLists.get(MAP_KEY1));
+        log("after sort::%s%n", mapOfLists);
+    }
+
+    private static void log(String pattern, Object ... args) {
+        System.out.printf(pattern, args);
+    }
+
+}
