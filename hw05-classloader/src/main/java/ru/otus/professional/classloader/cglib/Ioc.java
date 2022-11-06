ca4acf28501abf97c7fbba6d614de8beb1514645
17876388 <alchin.y.y@sberbank.ru>
2022-11-06 12:34:18 +0300

    hw05-cglib. Commit notes: move reflection to constructor

--- hw05-classloader/src/main/java/ru/otus/professional/classloader/cglib/Ioc.java
+++ hw05-classloader/src/main/java/ru/otus/professional/classloader/cglib/Ioc.java
@@ -1,16 +1,9 @@
 package ru.otus.professional.classloader.cglib;
 
 import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
-import ru.otus.professional.classloader.annotation.Log;
 
-import java.lang.reflect.Method;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-@Slf4j(topic = "proxy-logging")
 @UtilityClass
 public class Ioc {
 
@@ -18,34 +11,10 @@ public class Ioc {
     public <T> T createInstance(Class<T> clazz) {
         try {
             T testLogging = clazz.getDeclaredConstructor().newInstance();
-            MethodInterceptor handler = createMethodInterceptor(testLogging);
+            MethodInterceptor handler = new LogMethodInterceptor(testLogging);
             return (T) Enhancer.create(TestLogging.class, handler);
         } catch (Exception ex) {
             throw new IocException("Cannot create proxy: " + ex.getMessage(), ex);
         }
-    }
-
-    private MethodInterceptor createMethodInterceptor(Object originalObject) {
-        return (obj, method, methodArgs, proxy) -> {
-            if (method.isAnnotationPresent(Log.class)) {
-                logInfoAboutMethod(method, methodArgs);
-                return method.invoke(originalObject, methodArgs);
-            }
-            return method.invoke(originalObject, methodArgs);
-        };
-    }
-
-    private void logInfoAboutMethod(Method method, Object[] args) {
-        log.info("executed |METHOD:{} |PARAMS:{} |RESULT:{} |",
-                method.getName(),
-                createArgsString(args),
-                method.getReturnType().getSimpleName()
-        );
-    }
-
-    private String createArgsString(Object[] args) {
-        return Stream.of(args)
-                .map(Object::toString)
-                .collect(Collectors.joining(","));
     }
 }
